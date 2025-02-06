fb472b1ebdacc9994981f31b2481c8b4a8848519
VICTOR <olamuyiwavictor55@gmail.com>
2025-02-06 16:58:50 +0100

    responsiveness

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,42 +1,42 @@
 "use client";
 import { useEffect, useState } from 'react';
 import { useParams } from 'next/navigation';
-import { commercial, residentials, educational } from '@/app/constants';
+import { commercial, residentials, educational, COMPLETED } from '@/app/constants';
 import PageCard from '@/app/components/PageCard';
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/app/components/ui/breadcrumb";
 
 const ProjectPage = () => {
-  const params = useParams();
-  const project = Array.isArray(params.project) ? params.project[0] : params.project;
-  const allProperties = [
-    ...commercial.map((item) => ({ ...item, propertyType: item.propertyType || 'commercial' })),
-    ...residentials.map((item) => ({ ...item, propertyType: item.propertyType || 'residential' })),
-    ...educational.map((item) => ({ ...item, propertyType: item.propertyType || 'educational' })),
-  ];
+//   const params = useParams();
+//   const project = Array.isArray(params.project) ? params.project[0] : params.project;
+//   const allProperties = [
+//     ...commercial.map((item) => ({ ...item, propertyType: item.propertyType || 'commercial' })),
+//     ...residentials.map((item) => ({ ...item, propertyType: item.propertyType || 'residential' })),
+//     ...educational.map((item) => ({ ...item, propertyType: item.propertyType || 'educational' })),
+//   ];
 
-  interface PropertyData {
-    id: number;
-    title: string;
-    description: string;
-    icon: string;
-    location: string;
-    route: string;
-    propertyType: string;
-    images: { src: string; alt: string; title: string; firstButton: string; secondButton: string; firstColour: string; secondColour: string; }[];
-  }
+//   interface PropertyData {
+//     id: number;
+//     title: string;
+//     description: string;
+//     icon: string;
+//     location: string;
+//     route: string;
+//     propertyType: string;
+//     images: { src: string; alt: string; title: string; firstButton: string; secondButton: string; firstColour: string; secondColour: string; }[];
+//   }
 
-  const [propertyData, setPropertyData] = useState<PropertyData[]>([]);
+//   const [propertyData, setPropertyData] = useState<PropertyData[]>([]);
 
-  useEffect(() => {
-    const filteredProperties = allProperties.filter(
-      (item) => project && item.propertyType.toLowerCase() === project.toLowerCase()
-    );
-    setPropertyData(filteredProperties);
-  }, [project]);
+//   useEffect(() => {
+//     const filteredProperties = allProperties.filter(
+//       (item) => project && item.propertyType.toLowerCase() === project.toLowerCase()
+//     );
+//     setPropertyData(filteredProperties);
+//   }, [project]);
 
-  if (!propertyData.length) {
-    return <div>Loading...</div>;
-  }
+//   if (!propertyData.length) {
+//     return <div>Loading...</div>;
+//   }
 
   return (
     <div className='px-20 mt-20 overflow-hidden text-white'>
@@ -56,11 +56,11 @@ const ProjectPage = () => {
         </Breadcrumb>
 
         <h1 className='text-2xl text-[#A08C5B] capitalize py-7'>Completed Projects</h1>
-        <p className="text-gray-400 py-7">{propertyData.length} properties</p>
+        <p className="text-gray-400 py-7">{COMPLETED.length} properties</p>
 
         {/* Grid container for property cards */}
         <div className='grid items-center justify-center w-full h-full grid-cols-3 gap-3'>
-          {propertyData.map((item) => (
+          {COMPLETED.map((item) => (
             <PageCard
               id={item.id}
               key={item.id}
@@ -69,8 +69,8 @@ const ProjectPage = () => {
               route={`/property/${item.title.toLowerCase().replace(/ /g, '-')}`}
               description={item.description}
               title={item.title}
-              property={project}
-              status={project}
+              property={item.propertyType}
+              status="completed"
             />
           ))}
         </div>
