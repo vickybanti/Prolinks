96a184163372407c41c96a7bac3bffa2d07843c9
VICTOR <olamuyiwavictor55@gmail.com>
2025-02-06 12:04:10 +0100

    error handling

--- app/components/ui/carousel.tsx
+++ app/components/ui/carousel.tsx
@@ -22,7 +22,7 @@ const Slide = ({ slide, index, current, handleSlideClick }: SlideProps) => {
 
   const xRef = useRef(0);
   const yRef = useRef(0);
-  const frameRef = useRef<number>();
+  const frameRef = useRef<number | null>(null);
 
   useEffect(() => {
     const animate = () => {
