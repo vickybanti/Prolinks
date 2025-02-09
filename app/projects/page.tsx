e5c39a9f30c1cba50d91aa874443b547de2416be
VICTOR <olamuyiwavictor55@gmail.com>
2025-02-09 23:27:04 +0100

    responsiveness

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -41,7 +41,7 @@ const isDesktop = useMediaQuery("(min-width: 768px)");
 
 
   return (
-    <div className={`px-32 py-44 overflow-hidden text-white ${!isDesktopo && 'work'}`}>
+    <div className={`px-32 py-44 overflow-hidden text-white ${!isDesktop && 'work'}`}>
       <div className='px-10'>
         <Breadcrumb>
           <BreadcrumbList>
@@ -61,7 +61,7 @@ const isDesktop = useMediaQuery("(min-width: 768px)");
         <p className="text-gray-400 py-7">{COMPLETED.length} properties</p>
 
         {/* Grid container for property cards */}
-        <div className={`grid items-center justify-center w-full h-full grid-cols-3 gap-3  ${!isDesktopo && 'projectGrid'}`}>
+        <div className={`grid items-center justify-center w-full h-full grid-cols-3 gap-3  ${!isDesktop && 'projectGrid'}`}>
           {COMPLETED.map((item) => (
             <PageCard
               id={item.id}
