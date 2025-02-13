48e54bc798fa0728cac0fd258ea8a51c7975fbf2
VICTOR <olamuyiwavictor55@gmail.com>
2025-02-13 23:06:38 +0100

    services animation

--- app/components/MobileNav.tsx
+++ app/components/MobileNav.tsx
@@ -24,7 +24,7 @@ import { useMediaQuery } from "../hooks/use-media-query";
 
 const MobileNav = () => {
   const pathname = usePathname();
-  const isDesktop = useMediaQuery("(min-width: 378px)");
+  const isDesktop = useMediaQuery("(min-width: 414px)");
 
   return (
     <div>
