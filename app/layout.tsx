6328d93d0c2075d8f89c38b9c896e352d6b9007b
VICTOR <olamuyiwavictor55@gmail.com>
2025-02-17 16:47:10 +0100

    changed favicon and description

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ const poppins = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "Prolinks",
+  title: "Prolinks - Architectural Excellence.",
   description: "Architecture. Engineering. Construction.",
 };
 
