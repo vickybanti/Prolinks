2a5ea6ef45fa3d16126fbc017ed1e27dac4167d3
VICTOR <olamuyiwavictor55@gmail.com>
2025-02-17 16:13:46 +0100

    changed image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,19 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Poppins } from "next/font/google";
-import {Navbar} from "./components/Navbar";
+import { Navbar } from "./components/Navbar";
 import Footer from "./components/Footer";
-
-
-
-
-
+import Head from "next/head";
 
 const poppins = Poppins({
- weight: ['400', '600'],
- subsets: ['latin'],
- display: 'swap',
+  weight: ['400', '600'],
+  subsets: ['latin'],
+  display: 'swap',
 });
 
 export const metadata: Metadata = {
   title: "Prolinks",
-  description: "",
+  description: ".Architecture .Engineering  .Construction",
 };
 
 export default function RootLayout({
@@ -27,8 +23,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <title>Prolinks</title>
+        <meta name="description" content=".Architecture .Engineering .Construction" />
+        <link rel="icon" href="/assets/logo/logo3.jpg" type="image/jpeg" />
+      </Head>
       <body
-        className={`${poppins.className} antialiased bg-white dark:bg-black text-black dark:text-white`}
+        className={`${poppins.className} antialiased bg-white dark:bg-white text-black dark:text-white`}
       >
         <Navbar />
         {children}
